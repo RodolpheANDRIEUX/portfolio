392da06eb05b5ccb0c8ca6caa3f4b8ee8f5eb2a6
Rodolphe <rodolpheandrieux@gmail.com>
2024-08-31 16:56:49 +0200

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,3 +12,7 @@
 	<source media="(min-width: 1000px)" type="image/webp" srcset={coffeeWebp} />
 	<img aria-hidden="true" class="bg-decorations coffee1" src={coffeePng} alt="coffee" />
 </picture>
+<picture>
+	<source media="(min-width: 1000px)" type="image/webp" srcset="/src/lib/images/coffee2.webp" />
+	<img aria-hidden="true" class="bg-decorations coffee" src="/src/lib/images/coffee2.png" alt="more coffee" />
+</picture>
