57deb4c43cb6f5efe7ad4545d196c380f95ecaed
Rodolphe <rodolpheandrieux@gmail.com>
2024-08-31 16:26:01 +0200

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,13 @@
 <script>
-	export let data;
-	import { formatDate } from '$lib/Utils.js';
-
-	function firstP(content) {
-		const match = content.match(/<p>.*?<\/p>/);
-		return match ? match[0] : '';
-	}
+	import images from '$lib/images/';
 </script>
 
 <svelte:head>
 	<title>Blog</title>
 	<meta name="description" content="Un blog accessible de fou" />
 </svelte:head>
+
+<picture>
+	<source media="(min-width: 1000px)" type="image/webp" srcset="{images}/coffee.webp" />
+	<img aria-hidden="true" class="bg-decorations coffee1" src="{images}/coffee.png" alt="coffee" />
+</picture>
