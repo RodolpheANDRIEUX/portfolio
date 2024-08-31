016924ecf09daba3acad3fe35d02688f4ef0f555
Rodolphe <rodolpheandrieux@gmail.com>
2024-08-31 16:28:38 +0200

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,14 @@
 <script>
-	import images from '$lib/images/';
+	import coffeeWebp from '$lib/images/coffee.webp';
+	import coffeePng from '$lib/images/coffee.png';
 </script>
 
 <svelte:head>
 	<title>Blog</title>
-	<meta name="description" content="Un blog accessible de fou" />
+	<meta name="" content="" />
 </svelte:head>
 
 <picture>
-	<source media="(min-width: 1000px)" type="image/webp" srcset="{images}/coffee.webp" />
-	<img aria-hidden="true" class="bg-decorations coffee1" src="{images}/coffee.png" alt="coffee" />
+	<source media="(min-width: 1000px)" type="image/webp" srcset={coffeeWebp} />
+	<img aria-hidden="true" class="bg-decorations coffee1" src={coffeePng} alt="coffee" />
 </picture>
